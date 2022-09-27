7784734b6dd1209c707d78ff4e6b0dbf97971eb9
Q-LMQ <12012919@mail.sustech.edu.cn>
2022-09-27 15:58:31 +0800

    upload Lab3 Naive Bayes

--- Lab/Lab3/lab3_script.py
+++ Lab/Lab3/lab3_script.py
@@ -117,8 +117,6 @@ X_test = np.array(X_test)
 X_train = np.array(X_train)
 X_train.shape
 
-
-
 # %%
 
 ########### Write Your Code Here ###########
@@ -130,59 +128,60 @@ from xml.sax.saxutils import prepare_input_source
 class NaiveBayes():
 
     def fit(self, X, y):
+        # 防止后续取对数的时候值为0，此处加上一个极小数
+        epsilon = 1e-8
         y = y.astype(int)
         self.X = X
         self.y = y
         self.classes = np.unique(y)
         
         self.parameters = {}
-        self.no_cnt = 0
-        self.yes_cnt = 0
-
-        self.no2word = np.array([0]*3000)
-        self.yes2word = np.array([0]*3000)   
-
+        
         for i, c in enumerate(self.classes):
             # Calculate the mean, variance, prior probability of each class
+            # 获取对应类的列
             X_Index_c = X[np.where(y == c)]
-            for x in X_Index_c:
-                for j in range(3000):
-                    if i == 0: # No
-                        self.no2word[j] += 1
-                        self.no_cnt += 1
-                    else: # yes
-                        self.yes2word[j] += 1
-                        self.yes_cnt += 1
             
-
             X_index_c_mean = np.mean(X_Index_c, axis=0, keepdims=True)
             X_index_c_var = np.var(X_Index_c, axis=0, keepdims=True)
+            X_index_c_var += epsilon
             parameters = {"mean": X_index_c_mean, "var": X_index_c_var, "prior": X_Index_c.shape[0] / X.shape[0]}
             
             self.parameters["class" + str(c)] = parameters
         
 
         # print(self.parameters)
+    @staticmethod
+    def get_first(var):
+        return -0.5 * np.log(2 * np.pi * var)
+    @staticmethod
+    def get_second(x, var, mean):
+        return -np.square(x - mean) / (2 * var)
 
-
-    def predict(self, whole_X):
+    def predict(self, X):
         # return class with highest probability，给X的每条记录都进行预测
       
         prediction = []
-        
-        for X in whole_X:
-            yes_prob = 0.5
-            no_prob = 0.5
-            for i in range(3000):
-                if X[i] == 1:
-                    yes_prob *= (self.yes2word[i] + 0.5)/(self.yes_cnt + 1)
-                    no_prob *= (self.no2word[i] + 0.5)/(self.no_cnt + 1)
+        for index in range(len(X)):
+            proba_array = []
+            for class_idx in self.parameters.keys():
+                tmp = []
+                parameter_dic = self.parameters[class_idx]
+                for feature in range(len(X[index])):
+                    var = parameter_dic["var"][0][feature]
+                    mean = parameter_dic["mean"][0][feature]
+                    tmp.append(NaiveBayes.get_first(var=var))
+                    tmp.append(NaiveBayes.get_second(X[index][feature], var=var, mean=mean))
+                    tmp.append(np.log(parameter_dic['prior']))
+                proba_array.append(np.array(tmp).sum())
 
-            if yes_prob > no_prob:
-                prediction.append(1)
-            else:
+            # print("proba_array = ", proba_array)
+            if proba_array[0] > proba_array[1]:
                 prediction.append(0)
+            else:
+                prediction.append(1)
 
+                
         return prediction
 
 
@@ -197,6 +196,57 @@ model = NaiveBayes()
 model.fit(X_train,y_train) 
 
 result = model.predict(X_test)
+# result
 print (confusion_matrix(y_test, result))
+
+# %% [markdown]
+# ### 6) Checking the results on test set
+# The test set contains 130 spam emails and 130 non-spam emails. Please compute accuracy, recall, F-1 score to evaluate the performance of your spam filter.
+
+# %%
+########### Write Your Code Here ###########
+from sklearn.metrics import accuracy_score, recall_score, f1_score
+print("The accuracy score of my Bayes model is ", accuracy_score(y_test, result))
+print("The recall score of my Bayes model is ", recall_score(y_test, result))
+print("The f1 score of my Bayes mode is ", f1_score(y_test, result))
+
+############################################
+
+# %% [markdown]
+# ### Exercise 2 Compare your Naïve Bayes algorithm with GassianNB from Sklearn, which one does better? Where is the gap?
+
+# %%
+########### Write Your Code Here ###########
+from sklearn.naive_bayes import GaussianNB
+model_GaussanNB = GaussianNB()
+model_GaussanNB.fit(X_train,y_train)
+y_predict = model_GaussanNB.predict(X_test)
+print("accuracy of sklearn is", accuracy_score(y_test,y_predict))
+
+############################################
+
+# %% [markdown]
+# The accuracy of my model is the same as the sklearn, both are 0.9615384615384616
+
+# %% [markdown]
+# ### Exercise 3 Questions
+# 1. Describe another real-world application where the naïve Bayes method can be applied
+# 
+#     The Naive Bayes method can also be use in predicting the price of house considering the features such as location, area, floor, etc..
+# 
+# 2. What are the strengths of the naïve Bayes method; when does it perform well?
+# 
+#     The strengths of the naive Bayes method is that the efficiency is very stable, and it's not sensible to the missing data.
+# 
+#     When the relationship between features is independent, and the sample size is small, the performance is well.
+# 
+# 3. What are the weaknesses of the naïve Bayes method; when does it perform poorly?
+# 
+#     The weaknesses of the naive Bayes method is that it is strongly dependent on the independency of features. When the features have strong dependency with each other, the performance is poor. 
+# 
+# 
+# 4. What makes the naïve Bayes method a good candidate for the classification problem, if you have enough knowledge about the data?
+# 
+#     The Naive Bayes has strong interpretability and it is of strong use when the sample size is small and the features are independent.
 
 
